613cd56161fde122f14374bc629fa4a2c4a9120d
Alexander Shmakov <shmakov.sasha4861@gmail.com>
2020-10-09 11:04:20 +0300

    map data

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,24 +3,32 @@ import classes from './Dialogs.module.css'
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
 
-
-
 function Dialogs() {
+
+    let dialogs = [
+        {id: 1, name: "Dimych"},
+        {id: 2, name: "Sanek"},
+        {id: 3, name: "Masha"},
+        {id: 4, name: "Anya"},
+    ]
+
+    let messages = [
+        {id: 1, message: "hello!"},
+        {id: 2, message: "How do you feel?!"},
+        {id: 3, message: "Su4ka!"},
+        {id: 4, message: "I love you!"},
+    ]
+
     return (
         <div>
             <div className={classes.dialogs}>
                 <div className={classes.dialogItems}>
-                    <DialogItem name="Andrey" id="1" />
-                    <DialogItem name="Sanek" id="2" />
-                    <DialogItem name="Anechka" id="3" />
+                    {dialogs.map(item => <DialogItem name={item.name} id={item.id}/>)}
                 </div>
                 <div className={classes.messages}>
-                    <Message message="Hi" />
-                    <Message message="How do you feel?" />
-                    <Message message="Su4ka" />
+                    {messages.map(item => <Message message={item.message} id={item.id}/>)}
                 </div>
             </div>
-
         </div>
     );
 }
